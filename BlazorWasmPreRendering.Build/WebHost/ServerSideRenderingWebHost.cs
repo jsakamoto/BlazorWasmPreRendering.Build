5065b93e6e3483d427db647d457af43e40958df3
jsakamoto <junichi.jp@gmail.com>
2022-09-22 23:53:59 +0900

    Stabilize unit test.

--- BlazorWasmPreRendering.Build/WebHost/ServerSideRenderingWebHost.cs
+++ BlazorWasmPreRendering.Build/WebHost/ServerSideRenderingWebHost.cs
@@ -13,12 +13,17 @@ namespace Toolbelt.Blazor.WebAssembly.PrerenderServer.WebHost
 {
     internal class ServerSideRenderingWebHost
     {
-        public static async Task<IWebHost> StartWebHostAsync(CustomAssemblyLoader assemblyLoader, string? environment, string serverPort, BlazorWasmPrerenderingOptions prerenderingOptions)
+        internal static int GetAvailableTcpPort(string tcpPortRangeText)
         {
             var usedTcpPorts = IPGlobalProperties.GetIPGlobalProperties().GetActiveTcpListeners().Select(listener => listener.Port).ToHashSet();
-            var availabeTcpPort = IntEnumerator.ParseRangeText(serverPort).FirstOrDefault(port => !usedTcpPorts.Contains(port));
-            if (availabeTcpPort == 0) throw new Exception($"There is no avaliable TCP port in range \"{serverPort}\".");
+            var availabeTcpPort = IntEnumerator.ParseRangeText(tcpPortRangeText).FirstOrDefault(port => !usedTcpPorts.Contains(port));
+            if (availabeTcpPort == 0) throw new Exception($"There is no avaliable TCP port in range \"{tcpPortRangeText}\".");
+            return availabeTcpPort;
+        }
 
+        public static async Task<IWebHost> StartWebHostAsync(CustomAssemblyLoader assemblyLoader, string? environment, string serverPort, BlazorWasmPrerenderingOptions prerenderingOptions)
+        {
+            var availabeTcpPort = GetAvailableTcpPort(serverPort);
             var baseAddress = $"http://127.0.0.1:{availabeTcpPort}";
             var hostEnvironment = new HostEnvironment(baseAddress, environment ?? "Prerendering");
 
@@ -29,6 +34,7 @@ namespace Toolbelt.Blazor.WebAssembly.PrerenderServer.WebHost
                 .AddJsonFile(appsettingsEnvironmentPath, optional: true)
                 .Build();
 
+            var webHost = default(IWebHost);
             var hostBuilder = new WebHostBuilder()
                 .UseConfiguration(configuration)
                 .UseKestrel()
@@ -36,9 +42,12 @@ namespace Toolbelt.Blazor.WebAssembly.PrerenderServer.WebHost
                 .UseWebRoot(prerenderingOptions.WebRootPath)
                 .ConfigureServices(services => services
                     .AddSingleton(assemblyLoader)
-                    .AddSingleton(hostEnvironment as IWebAssemblyHostEnvironment))
+                    .AddSingleton(hostEnvironment as IWebAssemblyHostEnvironment)
+                    // NOTICE: The next line is important to expose the IWebHost object to the HTTP request handler. 
+                    //         For more detail, see the source code inside the "Startup.cs".
+                    .AddTransient(_ => webHost ?? throw new NullReferenceException()))
                 .UseStartup(context => new Startup(context.Configuration, new Uri(baseAddress), hostEnvironment, prerenderingOptions));
-            var webHost = hostBuilder.Build();
+            webHost = hostBuilder.Build();
             await webHost.StartAsync();
             return webHost;
         }
