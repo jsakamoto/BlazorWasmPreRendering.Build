978eb9db99dda137813e855ec170073fc9066c40
jsakamoto <junichi.jp@gmail.com>
2022-10-22 11:41:39 +0900

    Make the all test to be passed

---
+++ BlazorWasmPreRendering.Build.Test/SetUpFixture.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using Toolbelt;
+using Toolbelt.Diagnostics;
+
+namespace BlazorWasmPreRendering.Build.Test;
+
+[SetUpFixture]
+public class SetUpFixture
+{
+    [OneTimeSetUp]
+    public async Task OneTimeSetup()
+    {
+        var slnDir = FileIO.FindContainerDirToAncestor("*.sln");
+        var webHostProjDir = Path.Combine(slnDir, "BlazorWasmPreRendering.Build.WebHost");
+        var targetDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ".webhost");
+        if (Directory.Exists(targetDir)) Directory.Delete(targetDir, recursive: true);
+
+        var dotnetCLI = await XProcess.Start(
+            "dotnet",
+            $"publish -c:Release -f:net6.0 -o:\"{targetDir}\"",
+            webHostProjDir)
+            .WaitForExitAsync();
+        dotnetCLI.ExitCode.Is(0, message: dotnetCLI.Output);
+
+        foreach (var file in Directory.GetFiles(targetDir, "Microsoft.*.dll")) File.Delete(file);
+        foreach (var file in Directory.GetFiles(targetDir, "web.config")) File.Delete(file);
+        foreach (var file in Directory.GetFiles(targetDir, "*.deps.json")) File.Delete(file);
+    }
+}
