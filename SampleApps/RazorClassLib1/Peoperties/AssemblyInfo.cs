3426c8af2b70f740a0924cef1b6b92905c6e972f
jsakamoto <junichi.jp@gmail.com>
2026-02-19 22:55:22 +0900

    Refine all sample projects

--- SampleApps/RazorClassLib1/Peoperties/AssemblyInfo.cs
+++ SampleApps/RazorClassLib1/Peoperties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyMetadata("BlazorWasmPreRendering.Build.MiddlewarePackageReference", "MiddlewarePackage1,,1.0.0")]
-[assembly: AssemblyMetadata("BlazorWasmPreRendering.Build.MiddlewarePackageReference", "MiddlewarePackage2,Middleware2,2.0.0")]
+[assembly: AssemblyMetadata("BlazorWasmPreRendering.Build.MiddlewarePackageReference", "MiddlewarePackage1,,8.0.0")]
+[assembly: AssemblyMetadata("BlazorWasmPreRendering.Build.MiddlewarePackageReference", "MiddlewarePackage2,Middleware2,8.0.0")]
