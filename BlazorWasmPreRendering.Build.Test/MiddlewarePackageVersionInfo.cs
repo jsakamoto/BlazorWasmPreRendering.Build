472f45e0827c8b32c6de1a45152c5b8360d44f9a
jsakamoto <junichi.jp@gmail.com>
2026-05-02 22:05:48 +0900

    Improve test stability by auto-generating middleware NuGet packages on demand

---
+++ BlazorWasmPreRendering.Build.Test/MiddlewarePackageVersionInfo.cs
@@ -0,0 +1,7 @@
+namespace BlazorWasmPreRendering.Build.Test;
+
+internal static class MiddlewarePackageVersionInfo
+{
+    internal static string MiddlewarePackage1Version = "1.0.0";
+    internal static string MiddlewarePackage2Version = "2.0.0";
+}
